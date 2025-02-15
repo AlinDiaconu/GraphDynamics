246393b96fdd1c890b054fd477b824b53d813935
Alin Diaconu <xeon.alin@yahoo.com>
2025-02-16 00:26:00 +0200

    Code improved

--- graph.py
+++ graph.py
@@ -2,21 +2,26 @@ import matplotlib.pyplot as plt
 import csv
 
 def create_data(file_path):
-    x = []
-    y = []
+    data = []
     with open(file_path, 'r') as csvfile:
         lines = csv.reader(csvfile, delimiter=',')
         for row in lines:
-            x.append(row[0])
-            y.append(int(row[1]))
-    return x, y
+            for i, value in enumerate(row):
+                if len(data) <= i:
+                    data.append([])
+                try:
+                    data[i].append(float(value))
+                except ValueError:
+                    data[i].append(value)
+    return data
 
-def draw_graph(x, y):
-    plt.plot(x, y, color='g', linestyle='dashed', marker='o', label="Weather Data")
+def draw_graph(data):
+    for i in range(1, len(data)):
+        plt.plot(data[0], data[i], linestyle='dashed', marker='o', label=f"Value {i}")
     plt.xticks(rotation=25)
-    plt.xlabel('Dates')
-    plt.ylabel('Temperature')
-    plt.title('Weather Report', fontsize=20)
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.title('GraphDynamics', fontsize=20)
     plt.grid()
     plt.legend()
     plt.show()
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from graph import create_data, draw_graph
 
 if __name__ == "__main__":
     file_path = 'data.csv'
-    x, y = create_data(file_path)
-    draw_graph(x, y)
+    data = create_data(file_path)
+    draw_graph(data)
